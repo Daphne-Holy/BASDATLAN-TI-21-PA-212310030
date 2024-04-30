6265861a9db673e735ca0f85405fcb2252ea209c
Daphne Holy <113779719+Daphne-Holy@users.noreply.github.com>
2024-04-30 12:54:46 +0700

    Add files via upload

---
+++ Modul 5 Playground pt 2.js
@@ -0,0 +1,38 @@
+use("db_film");
+
+// db.createCollection("ArrTest")
+
+// db.ArrTest.insert({
+//     id: 1,
+//     Nama: "Dolly",
+//     Nilai : [50, 30]
+// })
+
+// db.ArrTest.updateOne({id: 1}, {$push: {Nilai: 100}})
+// db.ArrTest.updateOne({id: 1}, {$pull: {Nilai: 100}})
+
+// db.ArrTest.updateOne({id: 1}, {$addToSet: {Nilai: 100}})
+
+// db.customers.find({Last_Name: {$in: ["Skywalker", "Ridley"]}})
+// db.customers.find({Last_Name: {$nin: ["Skywalker", "Ridley"]}})
+
+// db.film.insert([{
+//     id: 1,
+//     Title: "Sucker Punch",
+//     Description: "",
+// }, {
+//     id: 2,
+//     Title: "Anna Karenina",
+//     Description: ""
+// }, {
+//     id: 3,
+//     Title: "Conjuring",
+//     Description: ""
+// },
+// ])
+
+// db.film.find({Title: {$regex: /g$/, $options: 'm'}})
+
+// db.customers.find().limit(2)
+
+// db.film.find().sort({Title: 1})
